5bc8e5d0cbf681b15c7d10d3158feec0f42544ea
Hoa Hoang <hoa.hoang-1@ou.edu>
2020-02-12 13:37:56 -0600

    Fix lab 1 and 2

---
+++ Lab1/R/myf.R
@@ -0,0 +1,12 @@
+#' Square function
+#' Takes a vector and return a vector of squared components
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^2
+}
